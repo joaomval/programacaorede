4c5e21f1018352a174b56026a2fdc82fe94638fa
joaomval <joaomval@65ba6260-4025-a7c8-6d14-74ba33d99b03>
2010-05-13 19:48:40 +0000

    coiso

--- trunk/yGallery/src/java/Sistema/Url.java
+++ trunk/yGallery/src/java/Sistema/Url.java
@@ -2,15 +2,21 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Sistema;
 
 import BaseDados.Teste_Acesso_BD;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Url {
-        public static void insere(final Hashtable<String, Object> params) {
+
+    public static void insere(final Hashtable<String, Object> params) {
         Teste_Acesso_BD bd = new Teste_Acesso_BD();
         bd.carregaDriverEAbreConnection();
         bd.abreStatement();
@@ -23,5 +29,30 @@ public class Url {
             bd.fechaStatement();
             bd.fechaConnection();
         }
+    }
+
+    public static List<String> devolveURLsArtigo(String idArtigo) {
+        Teste_Acesso_BD bd = new Teste_Acesso_BD();
+        List<String> lista = new ArrayList<String>();
+        bd.carregaDriverEAbreConnection();
+        bd.abreStatement();
+        String qryName = new String("devolve_urls_por_id_artigo");
+        Hashtable params = new Hashtable();
+        params.put("var_idArtigo", idArtigo);
+        ResultSet rs = null;
+        try {
+            rs = bd.executeSelect(qryName, params);
+            while (rs.next()) {
+                lista.add(rs.getString("URL"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Url.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(Url.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            bd.fechaStatement();
+            bd.fechaConnection();
+        }
+        return lista;
     }
 }
